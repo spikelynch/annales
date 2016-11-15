20d431123fde41f0b8438b25b139709c55dfb528
Mike Lynch <mike.lynch@iinet.net.au>
2016-11-15 15:09:17 +1100

    Forgot to check in Annales.Omens

---
+++ app/Annales/Omens.hs
@@ -0,0 +1,20 @@
+module Annales.Omens (omen) where
+
+import TextGen (TextGen, runTextGen, word, choose, remove, list, randrep, rep, perhaps, smartjoin)
+
+import Annales.Empire ( TextGenCh, Empire, court, vocabGet, generate, dumbjoin, randn)
+import Annales.Deaths ( deathOf )
+  
+
+
+
+
+
+omen :: Empire -> IO ( Empire, TextGenCh )
+omen e = return ( e, list [ collective, phenom, word "in", place ] )
+  where phenom = vocabGet e "phenomena"
+        place = vocabGet e "places"
+
+
+collective :: TextGenCh
+collective = choose $ map word [ "Outbreak of", "Panic caused by", "Great", "Reports of", "Rumours of" ]
